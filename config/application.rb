e84e2a63fcb37aefdb7a4b11de59b5a6c077d635
Roy Vargas <royvargas55@gmail.com>
2020-05-30 03:30:59 -0600

    prepare test suite

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,13 @@ Bundler.require(*Rails.groups)
 
 module Dogsapi
   class Application < Rails::Application
+    config.generators do |g|
+      # Force new test files to be generated in the minitest-spec style
+      g.test_framework :minitest, spec: true
+
+      # Always use .js files, never .coffee
+      g.javascript_engine :js
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "minitest/rails"
+require "minitest/reporters"  # for Colorized output
+
+Minitest::Reporters.use!(
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
