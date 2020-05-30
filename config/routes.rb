23e244ccdf5ed6ed74e7d832aa3c45a32b2bc5fb
Roy Vargas <royvargas55@gmail.com>
2020-05-30 04:05:21 -0600

    test passed and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,10 @@
 Rails.application.routes.draw do
+    resources :dogs, only: [:index, :show, :create, :update, :destroy] do
+        resources :reservations
+    end
+    resources :reservations
+    get '/dog', to: 'dogs#search'
+    
+    get '/breeds', to: 'breeds#index'
+    get '/breed', to: 'breeds#search'
 end
