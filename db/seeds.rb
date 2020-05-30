41ecea08235b312f19974cbcef2f0b259c42d108
Roy Vargas <royvargas55@gmail.com>
2020-05-30 04:12:01 -0600

    seed db and get redis caching working properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do |n|
+    Breed.create(
+      breed: (Faker::Creature::Dog.unique.breed).downcase
+    )
+  end
+  
+  1000.times do |n|
+    Dog.create(
+      name:   Faker::Creature::Dog.name,
+      age:    Faker::Number.number(digits: 2),
+      weight: Faker::Number.number(digits: 2),
+      breeds: (Faker::Creature::Dog.breed).downcase
+    )
+  end
